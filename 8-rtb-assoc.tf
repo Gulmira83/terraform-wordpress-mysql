805533875b18949e80c078052acdd72262827d21
Huseyin Akten <husakten@gmail.com>
2019-12-23 15:28:33 -0600

    rtb manual id

--- 8-rtb-assoc.tf
+++ 8-rtb-assoc.tf
@@ -12,14 +12,14 @@ resource "aws_route_table_association" "pub-3" {
 }
 
 resource "aws_route_table_association" "priv-1" {
-  route_table_id = aws_vpc.nat-rtb.id
+  route_table_id = aws_vpc.rtb-0ffeddfef6c28a6e0
   subnet_id      = aws_subnet.priv-1.id
 }
 resource "aws_route_table_association" "priv-2" {
-  route_table_id = aws_vpc.nat-rtb.id
+  route_table_id = aws_vpc.rtb-0ffeddfef6c28a6e0
   subnet_id      = aws_subnet.priv-2.id
 }
 resource "aws_route_table_association" "priv-3" {
-  route_table_id = aws_vpc.nat-rtb.id
+  route_table_id = aws_vpc.rtb-0ffeddfef6c28a6e0
   subnet_id      = aws_subnet.priv-3.id
 }
