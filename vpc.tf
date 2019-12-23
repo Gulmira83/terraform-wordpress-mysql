bf4ba55d2abfdb00310af5edda8eab8162eb305e
Huseyin Akten <husakten@gmail.com>
2019-12-22 21:35:36 -0600

    vpc

---
+++ vpc.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_vpc" "vpc-ohio" {
+  cidr_block = "192.168.0.0/16"
+}
+
+resource "aws_internet_gateway" "igw-ohio" {
+  vpc_id = aws_vpc.vpc-ohio.id
+}
+
+resource "aws_route" "pub_rtb" {
+  route_table_id         = "${aws_vpc.vpc-ohio.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.igw-ohio.id}"
+}
