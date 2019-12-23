f1e928497dc4442c2a23889bb79adbc79b54ba84
Huseyin Akten <husakten@gmail.com>
2019-12-23 15:35:35 -0600

    correction on nat rtb

--- 9-nat-rtb-assoc.tf
+++ 9-nat-rtb-assoc.tf
@@ -1,12 +1,12 @@
 resource "aws_route_table_association" "priv-1" {
-  route_table_id = aws_vpc.rtb-0ffeddfef6c28a6e0.id
+  route_table_id = aws_vpc.vpc-ohio.nat-rtb.id
   subnet_id      = aws_subnet.priv-1.id
 }
 resource "aws_route_table_association" "priv-2" {
-  route_table_id = aws_vpc.rtb-0ffeddfef6c28a6e0.id
+  route_table_id = aws_vpc.vpc-ohio.nat-rtb.id
   subnet_id      = aws_subnet.priv-2.id
 }
 resource "aws_route_table_association" "priv-3" {
-  route_table_id = aws_vpc.rtb-0ffeddfef6c28a6e0.id
+  route_table_id = aws_vpc.vpc-ohio.nat-rtb.id
   subnet_id      = aws_subnet.priv-3.id
 }
