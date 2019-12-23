df6644a3adeb2dc95dc94bb9b6bd2bc1879f3673
Huseyin Akten <husakten@gmail.com>
2019-12-23 15:18:40 -0600

    edit

--- 8-rtb-assoc.tf
+++ 8-rtb-assoc.tf
@@ -1,11 +1,25 @@
-resource "aws_route_table_association" "pub" {
+resource "aws_route_table_association" "pub-1" {
   route_table_id = aws_vpc.vpc-ohio.main_route_table_id
-  subnet_id      = aws_subnet.pub-1.id,
-  subnet_id      = aws_subnet.pub-2.id,
-  subnet_id      = aws_subnet.pub-3.id,
+  subnet_id      = aws_subnet.pub-1.id
+}
+resource "aws_route_table_association" "pub-2" {
+  route_table_id = aws_vpc.vpc-ohio.main_route_table_id
+  subnet_id      = aws_subnet.pub-2.id
+}
+resource "aws_route_table_association" "pub-3" {
+  route_table_id = aws_vpc.vpc-ohio.main_route_table_id
+  subnet_id      = aws_subnet.pub-3.id
 }
 
-resource "aws_route_table_association" "nat" {
-  route_table_id = aws_vpc.vpc-ohio.id
+resource "aws_route_table_association" "priv-1" {
+  route_table_id = aws_vpc.nat-rtb.id
   subnet_id      = aws_subnet.priv-1.id
+}
+resource "aws_route_table_association" "priv-2" {
+  route_table_id = aws_vpc.nat-rtb.id
+  subnet_id      = aws_subnet.priv-2.id
+}
+resource "aws_route_table_association" "priv-3" {
+  route_table_id = aws_vpc.nat-rtb.id
+  subnet_id      = aws_subnet.priv-3.id
 }
