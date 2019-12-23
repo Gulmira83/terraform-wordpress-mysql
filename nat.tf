c027be22c8e3aadd8eeed30221b24db40768a925
Huseyin Akten <husakten@gmail.com>
2019-12-22 22:06:21 -0600

    nat and eip

--- nat.tf
+++ nat.tf
@@ -1,5 +1,8 @@
+resource "aws_eip" "eip" {
+  vpc      = true
+}
+
 resource "aws_nat_gateway" "nat-ohio" {
-  subnet_id = aws_subnet.pub-1.id
-tags = {
-    Name = "nat-ohio"
+  subnet_id     = aws_subnet.pub-1.id
+  allocation_id = aws_eip.eip.id
 }
