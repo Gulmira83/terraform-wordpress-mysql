9d8bbbe60a9b6a94d2fe0644abe7360d1ad717a9
Huseyin Akten <husakten@gmail.com>
2019-12-22 22:20:42 -0600

    edit

--- priv-rtb.tf
+++ priv-rtb.tf
@@ -1,8 +1,8 @@
 resource "aws_route_table" "r" {
   vpc_id = aws_vpc.vpc-ohio.id
-}
 
   route {
-    cidr_block = "10.0.0.0/24"
+    cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat-ohio.id
+  }
 }
