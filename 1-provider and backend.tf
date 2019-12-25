65fe0a82514fc43d4579f6a9a9042718c4fb33fb
Huseyin Akten <husakten@gmail.com>
2019-12-24 22:42:55 -0600

    puosh

--- 1-provider and backend.tf
+++ 1-provider and backend.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
   
 backend "s3" {
-  bucket = "terraform-tfstate-huseyin"
-  key    = "t2-terraform"
-  region = "us-east-1"
-  }
+bucket = "terraform-tfstate-huseyin"
+key    = "t2-terraform"
+region = "us-east-1"
+}
