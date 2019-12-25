604367b48140dd2f2a591b452440d6fb58b35d78
Huseyin Akten <husakten@gmail.com>
2019-12-24 22:50:21 -0600

    edit

--- 2-backend.tf
+++ 2-backend.tf
@@ -1,5 +1,7 @@
+terraform {
 backend "s3" {
-    bucket = "terraform-tfstate-huseyin"
-    key    = "t2-terraform"
-    region = "us-east-1"
-    }
+bucket = "terraform-tfstate-huseyin"
+key    = "t2-terraform"
+region = "us-east-1"
+}
+}
