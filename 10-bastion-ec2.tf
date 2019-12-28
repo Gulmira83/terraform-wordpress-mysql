c6525ac65b175b0462383315f311d07edf6a1018
Huseyin Akten <husakten@gmail.com>
2019-12-27 22:57:56 -0600

    sadf

--- 10-bastion-ec2.tf
+++ 10-bastion-ec2.tf
@@ -4,6 +4,6 @@ resource "aws_instance" "bastion2d" {
   subnet_id = aws_subnet.pub-1.id
   associate_public_ip_address = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = aws_security_group.ssh.name
+  vpc_security_group_ids = aws_security_group.ssh.id
 
 }
