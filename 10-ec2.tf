5e3c8d7a5fe0d8dd2086d6d07f6208dff258dbcd
Huseyin Akten <husakten@gmail.com>
2019-12-27 19:56:15 -0600

    edit

--- 10-ec2.tf
+++ 10-ec2.tf
@@ -1,13 +1,7 @@
-resource "aws_instance" "wp-instance" {
+resource "aws_instance" "bastion" {
   ami             = var.ami
   instance_type   = var.instance_type
   associate_public_ip_address = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["ssh"]
-  provisioner "remote-exec" {
-    connection {
-      host        = self.public_ip
-      type        = "ssh"
-      user        = var.user
-      private_key = file(var.ssh_key_location)
-      }
+}
