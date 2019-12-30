72e75a3dd1a41ac4d99114b0f4a4f29c0e518df0
Huseyin Akten <husakten@gmail.com>
2019-12-29 23:36:17 -0600

    db sg corrected

--- 10-sg.tf
+++ 10-sg.tf
@@ -54,6 +54,12 @@ resource "aws_security_group" "db" {
   description = "Allow db inbound traffic" 
   vpc_id      = aws_vpc.vpc-ohio.id
 
+ingress { 
+    from_port   = 22 
+    to_port     = 22 
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]   
+} 
   ingress { 
     from_port   = 3306
     to_port     = 3306
