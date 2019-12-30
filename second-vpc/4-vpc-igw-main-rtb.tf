93f05c8b16a75feae6f0fca8002a6586eff50fb8
Huseyin Akten <husakten@gmail.com>
2019-12-29 23:50:46 -0600

    vpcs foldered seperately

--- second-vpc/4-vpc-igw-main-rtb.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_vpc" "vpc-virginia" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = "true"
-}
-
-resource "aws_internet_gateway" "igw-virginia" {
-  vpc_id = aws_vpc.vpc-virginia.id
-}
-
