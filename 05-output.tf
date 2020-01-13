35e4b0217522d37c512966357bfdaf30700cc3bd
Huseyin Akten <husakten@gmail.com>
2020-01-12 21:27:15 -0600

    database added

--- 05-output.tf
+++ 05-output.tf
@@ -1,15 +1,15 @@
-# output "vpc-id" {
-#   value = aws_vpc.vpc.id
-# }
-# output "vpc-cidr-block" {
-#   value = aws_vpc.vpc.cidr_block
-# }
-# output "rds-endpoint" {
-#   value = aws_db_instance.rds.address
-# }
-# output "bastion" {
-#   value = aws_instance.bastion.public_ip
-# }
-# output "web" {
-#   value = aws_instance.web.public_ip
-# }
+output "vpc-id" {
+  value = aws_vpc.vpc.id
+}
+output "vpc-cidr-block" {
+  value = aws_vpc.vpc.cidr_block
+}
+output "rds-endpoint" {
+  value = aws_db_instance.rds.address
+}
+output "bastion" {
+  value = aws_instance.bastion.public_ip
+}
+output "web" {
+  value = aws_instance.web.public_ip
+}
