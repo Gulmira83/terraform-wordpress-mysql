ed9ba878f2589e3ba6b1fb320f16ecac976e929f
Huseyin Akten <husakten@gmail.com>
2020-01-12 20:49:24 -0600

    db changed

--- 25-mysql.tf
+++ 25-mysql.tf
@@ -8,5 +8,7 @@ resource "aws_db_instance" "rds" {
   username          = "wpuser"
   password          = "wpuser60641"
   parameter_group_name   = "default.mysql5.7"
-  db_subnet_group_name   = aws_subnet.priv-1.name
+  db_subnet_group_name   = [aws_subnet.priv-1.name]
+  vpc_security_group_ids = [aws_security_group.db.id]
+
 }
