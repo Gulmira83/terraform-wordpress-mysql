395ac7a77af59a9363350c0382f1cdee1a79026f
Huseyin Akten <husakten@gmail.com>
2020-01-12 21:28:47 -0600

    new outputs added

--- 05-output.tf
+++ 05-output.tf
@@ -4,12 +4,19 @@ output "vpc-id" {
 output "vpc-cidr-block" {
   value = aws_vpc.vpc.cidr_block
 }
-output "rds-endpoint" {
-  value = aws_db_instance.rds.address
-}
+
 output "bastion" {
   value = aws_instance.bastion.public_ip
 }
 output "web" {
   value = aws_instance.web.public_ip
+}
+output "rds-endpoint" {
+  value = aws_db_instance.rds.address
+}
+output "rds-endpoint" {
+  value = aws_db_instance.rds.username
+}
+output "rds-endpoint" {
+  value = aws_db_instance.rds.name
 }
