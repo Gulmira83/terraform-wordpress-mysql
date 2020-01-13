edc6be46b567bb53035f9cac182eac01c6584601
Huseyin Akten <husakten@gmail.com>
2020-01-12 21:48:54 -0600

    cleaned extra files

--- 05-output.tf
+++ 05-output.tf
@@ -4,7 +4,6 @@ output "vpc-id" {
 output "vpc-cidr-block" {
   value = aws_vpc.vpc.cidr_block
 }
-
 output "bastion" {
   value = aws_instance.bastion.public_ip
 }
@@ -20,7 +19,6 @@ output "rds-username" {
 output "rds-db-name" {
   value = aws_db_instance.rds.name
 }
-
 output "rds-db-password" {
   value = aws_db_instance.rds.password
 }
