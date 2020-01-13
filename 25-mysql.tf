881fec1dd29d1e3096ba242a72ba73f5bf67dff1
Huseyin Akten <husakten@gmail.com>
2020-01-12 20:14:22 -0600

    db changed

--- 25-mysql.tf
+++ 25-mysql.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "rds" {
   instance_class    = "db.t2.micro"
   name              = "mydb"
   username          = "wpuser"
-  password          = "wpuser"
+  password          = "wpuser12345"
   db_subnet_group_name   = aws_subnet.pub-2.id
   vpc_security_group_ids = [aws_security_group.db.id,aws_security_group.ssh.id]
 }
