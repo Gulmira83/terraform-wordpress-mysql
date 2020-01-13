14f53d69f4eed22b1ded58709f5fbee2db789a8b
Huseyin Akten <husakten@gmail.com>
2020-01-12 20:11:38 -0600

    database changed

---
+++ 25-mysql.tf
@@ -0,0 +1,12 @@
+resource "aws_db_instance" "rds" {
+  allocated_storage = 20
+  storage_type      = "gp2"
+  engine            = "mysql"
+  engine_version    = "5.7"
+  instance_class    = "db.t2.micro"
+  name              = "mydb"
+  username          = "wpuser"
+  password          = "wpuser"
+  db_subnet_group_name   = aws_subnet.pub-2.id
+  vpc_security_group_ids = [aws_security_group.db.id,aws_security_group.ssh.id]
+}
